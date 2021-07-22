426bf0aae1af2304428c323c2a921f94d599c364
ahmaruff <ahmadmaruf2701@gmail.com>
2021-07-22 17:43:39 +0700

    remove unused import (cupertino.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Ahmad Ma'ruf",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: HomePage(),
     );
